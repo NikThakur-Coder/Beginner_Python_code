b0676d00ccf52df6bbab3137bc89f8bbd8afef74
NikThakur-Coder <neerajgadia@gmail.com>
2021-07-04 10:26:06 +0530

    Change

--- Palindrome.py
+++ Palindrome.py
@@ -1,8 +1,10 @@
 #write a function that checks whether a word or a phrase is palindrome or Not
 
 def palindrome(s):
-
+    s=s.lower()
     a=s.replace(' ','')
 
     return a==a[::-1]
-JSCHkjldbvdnvf
+
+s=input('Enter a Word or Phrase: ')
+print(palindrome(s))
