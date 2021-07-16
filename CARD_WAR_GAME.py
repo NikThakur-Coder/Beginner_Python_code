f8319e1800da56bd57fd191f0824ce1e6d840976
NikThakur-Coder <neerajgadia@gmail.com>
2021-07-17 01:12:43 +0530

    GAME

--- CARD_WAR_GAME.py
+++ CARD_WAR_GAME.py
@@ -12,7 +12,7 @@ class Card:
         self.value = values[rank]
 
     def __str__(self):
-        return {self.rank} + ' of ' + {self.suit}
+        return f'{self.rank} of {self.suit}'
 
 class Deck:
     def __init__(self):
@@ -46,57 +46,55 @@ class Player:
     def __str__(self):
         return 'Player ' + {self.name} + ' has ' + str(len(self.all_cards)) + 'cards'
 
-
-player1 = Player('One')
-player2 = Player('Two')
+#game setup
+player_1 = Player('One')
+player_2 = Player('Two')
 new_deck = Deck()
 new_deck.shuffle()
 for x in range(26):
-    player1.add_cards(new_deck.deal_one())
-    player2.add_cards(new_deck.deal_one())
+    player_1.add_cards(new_deck.deal_one())
+    player_2.add_cards(new_deck.deal_one())
 game_on = True
 round_num = 0
 war_count = 0
 while game_on:
     round_num += 1
-    print('Round' + str(round_num))
-    if len(player1.all_cards) == 0:
+    print(f'Round {round_num}')
+    if len(player_1.all_cards) == 0:
         print('Player_1, out of cards! PLayer_2 Wins')
         game_on = False
         break
-    if len(player2.all_cards) == 0:
+    if len(player_2.all_cards) == 0:
         print('Player_2, out of cards! PLayer_1 Wins')
         game_on = False
         break
     player_1_cards = []
-    player_1_cards.append(player1.remove_one())
+    player_1_cards.append(player_1.remove_one())
     player_2_cards=[]
-    player_2_cards.append(player2.remove_one())
+    player_2_cards.append(player_2.remove_one())
     at_war = True
     while at_war:
         if player_1_cards[-1].value > player_2_cards[-1].value:
-            player1.add_cards(player_1_cards)
-            player1.add_cards(player_2_cards)
+            player_1.add_cards(player_1_cards)
+            player_1.add_cards(player_2_cards)
             at_war = False
         elif player_1_cards[-1].value < player_2_cards[-1].value:
-            player2.add_cards(player_1_cards)
-            player2.add_cards(player_2_cards)
+            player_2.add_cards(player_1_cards)
+            player_2.add_cards(player_2_cards)
+            at_war = False
         else:
             print('WAR!')
-            if len(player1.all_cards) < 3:
+            if len(player_1.all_cards) < 3:
                 print('Player1 unable to declare the war!')
                 print('Player2 win!')
                 game_on = False
                 break
-            elif len(player2.all_cards) < 3:
+            elif len(player_2.all_cards) < 3:
                 print('Player2 unable to declare the war!')
                 print('Player1 win!')
                 game_on = False
                 break
             else:
                 for num in range(3):
-                    player_1_cards.append(player1.remove_one())
-                    player_2_cards.append(player2.remove_one())
-
-
-#it needs a Fix right now.
+                    player_1_cards.append(player_1.remove_one())
+                    player_2_cards.append(player_2.remove_one())
